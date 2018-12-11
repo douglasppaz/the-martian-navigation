5bb77e04fcf108e549a7035cf1b342eb4774aa92
Douglas Paz <eu@douglaspaz.com>
2018-12-11 00:14:20 -0300

    Add static method rotate to class Probe

--- src/models.js
+++ src/models.js
@@ -6,11 +6,34 @@ class Probe {
     this.y = 0;
     this.matrixSize = matrixSize || [5, 5];
   }
+
+  static rotate(direction, value) {
+    return ({
+      [Probe.LEFT]: {
+        [Probe.TURN_LEFT]: Probe.DOWN,
+        [Probe.TURN_RIGTH]: Probe.UP,
+      },
+      [Probe.RIGTH]: {
+        [Probe.TURN_LEFT]: Probe.UP,
+        [Probe.TURN_RIGTH]: Probe.DOWN,
+      },
+      [Probe.UP]: {
+        [Probe.TURN_LEFT]: Probe.LEFT,
+        [Probe.TURN_RIGTH]: Probe.RIGTH,
+      },
+      [Probe.DOWN]: {
+        [Probe.TURN_LEFT]: Probe.RIGTH,
+        [Probe.TURN_RIGTH]: Probe.LEFT,
+      },
+    })[direction][value];
+  }
 }
 
 Probe.LEFT = 'E';
 Probe.RIGTH = 'D';
 Probe.UP = 'C';
 Probe.DOWN = 'B';
+Probe.TURN_LEFT = 'left';
+Probe.TURN_RIGTH = 'rigth';
 
 exports.Probe = Probe;
--- src/models.spec.js
+++ src/models.spec.js
@@ -39,4 +39,86 @@ describe('class Probe', () => {
       expect(instance.matrixSize[1]).to.equal(3);
     });
   });
+
+  describe('static rotate()', () => {
+    describe('direction is left', () => {
+      let fn;
+
+      beforeEach(() => {
+        fn = value => Probe.rotate(Probe.LEFT, value);
+      });
+
+      describe('turn left', () => {
+        it('returns down', () => {
+          expect(fn(Probe.TURN_LEFT)).to.equal(Probe.DOWN);
+        });
+      });
+
+      describe('turn rigth', () => {
+        it('returns up', () => {
+          expect(fn(Probe.TURN_RIGTH)).to.equal(Probe.UP);
+        });
+      });
+    });
+
+    describe('direction is rigth', () => {
+      let fn;
+
+      beforeEach(() => {
+        fn = value => Probe.rotate(Probe.RIGTH, value);
+      });
+
+      describe('turn left', () => {
+        it('returns up', () => {
+          expect(fn(Probe.TURN_LEFT)).to.equal(Probe.UP);
+        });
+      });
+
+      describe('turn rigth', () => {
+        it('returns down', () => {
+          expect(fn(Probe.TURN_RIGTH)).to.equal(Probe.DOWN);
+        });
+      });
+    });
+
+    describe('direction is up', () => {
+      let fn;
+
+      beforeEach(() => {
+        fn = value => Probe.rotate(Probe.UP, value);
+      });
+
+      describe('turn left', () => {
+        it('returns left', () => {
+          expect(fn(Probe.TURN_LEFT)).to.equal(Probe.LEFT);
+        });
+      });
+
+      describe('turn rigth', () => {
+        it('returns rigth', () => {
+          expect(fn(Probe.TURN_RIGTH)).to.equal(Probe.RIGTH);
+        });
+      });
+    });
+
+    describe('direction is down', () => {
+      let fn;
+
+      beforeEach(() => {
+        fn = value => Probe.rotate(Probe.DOWN, value);
+      });
+
+      describe('turn left', () => {
+        it('returns rigth', () => {
+          expect(fn(Probe.TURN_LEFT)).to.equal(Probe.RIGTH);
+        });
+      });
+
+      describe('turn rigth', () => {
+        it('returns left', () => {
+          expect(fn(Probe.TURN_RIGTH)).to.equal(Probe.LEFT);
+        });
+      });
+    });
+  });
 });
