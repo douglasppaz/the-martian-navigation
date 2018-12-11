d2406758978132944f8767467cf0fb3f080cf60d
Douglas Paz <eu@douglaspaz.com>
2018-12-11 02:21:56 -0300

    Add error handling

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const { Probe } = require('./models');
 const { probeSerializer } = require('./utils');
+const { LeftTheMatrix, InvalidCommand } = require('./errors');
 
 
 module.exports = () => {
@@ -24,6 +25,29 @@ module.exports = () => {
 
   app.get('/ping/', (request, response) => {
     response.send('OK');
+  });
+
+  app.use((error, request, response, next) => {
+    if (error) {
+      if (error instanceof LeftTheMatrix) {
+        response.status(400);
+        response.json({
+          error: 'Commands not executed! Probe left the matrix.',
+        });
+      } else if (error instanceof InvalidCommand) {
+        response.status(400);
+        response.json({
+          error: 'List with invalid command...',
+        });
+      } else {
+        response.status(500);
+        response.json({
+          error,
+        });
+      }
+    } else {
+      next();
+    }
   });
 
   return app;
